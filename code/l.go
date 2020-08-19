b64a78717304405a36dea6e6554c6acd61b6dc04
Sheerlore <noema01@outlook.jp>
2020-08-19 19:56:02 +0900

    add io.Reader section

---
+++ code/l.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF") //Ctrl + D (C)
+			break
+		}
+		fmt.Printf("size=%d input='%s' \n", size, string(buffer))
+	}
+}
+
+// $ go run l.go < l.go
+//のようにすると自分自身のソースコードを決まったバッファサイズに区切って表示する。
+
